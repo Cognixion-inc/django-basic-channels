49094a99b9fde45ba8e6086e38df8f5377cef1ad
Alina Tariq <alina.tariq@ryerson.ca>
2022-09-14 14:31:20 -0400

    refactored code for readability

--- mywebsite/asgi.py
+++ mywebsite/asgi.py
@@ -12,7 +12,8 @@ import os
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-import chat.routing
+import kishuovert.routing
+import kishuovert
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mywebsite.settings')
 
@@ -20,7 +21,7 @@ application = ProtocolTypeRouter({
     'http':get_asgi_application(),
     'websocket':AuthMiddlewareStack(
         URLRouter(
-            chat.routing.websocket_urlpatterns
+            kishuovert.routing.websocket_urlpatterns
         )
     )
 })
